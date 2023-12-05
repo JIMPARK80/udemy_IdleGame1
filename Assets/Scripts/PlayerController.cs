ed201658c401114dbaa9ea62b7dcd2fdada3aaf0
Jinsung Park <van0711@gmail.com>
2023-12-05 06:52:21 -0500

    1. Change Camera Setting (Add Cinemachine Virtual Camera) 2. Fixing the character being pushed back (Rigid bodytype: Dynamic -> Static)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public Animator animator;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         Vector3 moveInput = new Vector3(0f, 0f, 0f);
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
